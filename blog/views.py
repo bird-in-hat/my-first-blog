ff26bd16ca85b3218b3c595ff1ef169cd326600f
bird-in-hat <buldakov.95@mail.ru>
2017-07-10 11:43:51 +0300

    Add styles

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.utils.timezone import now
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publish_date__lte=now()).order_by('publish_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
